c1ebf4c2e80d35f97e63c4d4a4feaf2a6c366866
gurnben <gurney.buchanan@gmail.com>
2018-04-17 10:57:29 -0400

    Removed MEAN code.

--- MEAN/public/app/chat/chat.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { ChatComponent } from './chat.component.js';
-
-export const ChatRoutes: Routes = [{
-  path: 'chat',
-  component: ChatComponent
-}];
